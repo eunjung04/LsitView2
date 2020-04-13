c2cb8c50fbfd49b3ab65aa5cf469d1c564cff5f7
eunjung04 <peunj04@gmail.com>
2020-04-13 22:02:19 +0900

    어댑터 리스트뷰 연결까지

--- app/src/main/java/com/example/lsitview2/MainActivity.kt
+++ app/src/main/java/com/example/lsitview2/MainActivity.kt
@@ -5,8 +5,26 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    var mAppAdapter:AppAdapter?=null
+
+    val appList=ArrayList<App>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        appList.add(App("아스팔트 8 : 에어본", "GameLoft", 5))
+        appList.add(App("MineCraft", "Mojang", 4))
+        appList.add(App("아스팔트 7 : 히트", "GameLoft", 4))
+        appList.add(App("팔라독 (Paladog)", "FazeCat", 2))
+        appList.add(App("Plants vs. Zombies", "EA Games", 3))
+        appList.add(App("스왐피 (Swampy)", "Disney", 5))
+        appList.add(App("리니지2M", "NCSOFT", 3))
+        appList.add(App("리니지M", "NCSOFT", 4))
+        appList.add(App("AFK아레나", "Lilith Games", 1))
+
+
+        mAppAdapter=AppAdapter(this, R.layout.app)
+
     }
 }
