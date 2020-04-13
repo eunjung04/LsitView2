8616e2eb33293ebda9da23b6d4c5556644995291
eunjung04 <peunj04@gmail.com>
2020-04-13 21:46:54 +0900

    어뎁터 기초 작업

--- app/src/main/java/com/example/lsitview2/AppAdapter.kt
+++ app/src/main/java/com/example/lsitview2/AppAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 
 class AppAdapter(context: Context, resId:Int, list:ArrayList<App>): ArrayAdapter<App>(context,resId, list){
 
@@ -15,10 +16,17 @@ class AppAdapter(context: Context, resId:Int, list:ArrayList<App>): ArrayAdapter
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var tempRow = convertView
-        if (tempRow == null) {
-            tempRow=inf.inflate(R.layout.app_list__item)
+        tempRow?.let {
 
+        }.let {
+            tempRow=inf.inflate(R.layout.app_list_item, null)
         }
+
+
+
+        val row=tempRow!!
+
+        val price=row.findViewById<TextView>(R.id.)
 
     }
 
