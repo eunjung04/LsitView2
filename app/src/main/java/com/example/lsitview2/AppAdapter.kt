80276b2cbb5be1e0e837756c5246a260d6321941
eunjung04 <peunj04@gmail.com>
2020-04-13 22:19:43 +0900

    실제 데이터 반영 -평점 찍는 용도

--- app/src/main/java/com/example/lsitview2/AppAdapter.kt
+++ app/src/main/java/com/example/lsitview2/AppAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.TextView
 
 class AppAdapter(context: Context, resId:Int, list:ArrayList<App>): ArrayAdapter<App>(context,resId, list){
@@ -19,14 +20,35 @@ class AppAdapter(context: Context, resId:Int, list:ArrayList<App>): ArrayAdapter
         tempRow?.let {
 
         }.let {
-            tempRow=inf.inflate(R.layout.app_list_item, null)
+            tempRow=inf.inflate(R.layout.con_list_item, null)
         }
 
 
 
         val row=tempRow!!
 
-        val price=row.findViewById<TextView>(R.id.)
+        val star1=row.findViewById<ImageView>(R.id.star1)
+
+        val star1=row.findViewById<TextView>(R.id.title)
+
+        title.text="${position+1}.${data.name}"
+        companyName.text=data.companyName
+
+
+        val star1List=ArrayList<ImageView>()
+        star1List.add(star1)
+        star1List.add(star2)
+        star1List.add(star3)
+        star1List.add(star4)
+        star1List.add(star5)
+
+        for (index in 0..4){
+            val star1=star1List.get(index)
+
+            if(index < star1List.get(index)
+        }
+
+        return row
 
     }
 
